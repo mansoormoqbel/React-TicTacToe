80ed4a7950dc7d3896ef502a3cbcc8774934efac
MANSOOR MOQBEL <mansoormoqbel124@outlook.com>
2025-11-04 23:23:26 +0300

    Initial project setup with Vite and React components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Mansoor from './mansoor'
+import './App.css'
+//import Tic from './TicTacToe'
+function App() {
+  const [count, setCount] = useState(0)
+ 
+  return (
+    <>
+      
+
+      <Mansoor/>
+      
+    </>
+  )
+  
+}
+export default App
